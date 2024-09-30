f682b8276f5cb6f61173b797667cb2aabb393fbf
Takuya Ueda <uedatakuya@gmail.com>
2024-09-30 22:30:42 +0900

    Fix overlay go version

--- cmd/testtime/testtime.go
+++ cmd/testtime/testtime.go
@@ -10,26 +10,44 @@ import (
 )
 
 var (
-	flagDir    string
-	flagUpdate bool
+	flagCacheDir string
+	flagUpdate   bool
 )
 
 func init() {
-	flag.StringVar(&flagDir, "dir", defaultDir(), "working directory for testtime")
+	flag.StringVar(&flagCacheDir, "dir", defaultCacheDir(), "cache directory for testtime")
 	flag.BoolVar(&flagUpdate, "u", false, "update exsiting files")
 }
 
 func main() {
 	flag.Parse()
-	overlay, err := createOverlay(flagUpdate, flagDir)
-	if err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(overlay)
 }
 
-func defaultDir() string {
+func run() error {
+
+	modroot := flag.Arg(0)
+	if modroot == "" {
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		modroot = wd
+	}
+
+	overlay, err := createOverlay(flagUpdate, modroot, flagCacheDir)
+	if err != nil {
+		return err
+	}
+	fmt.Println(overlay)
+
+	return nil
+}
+
+func defaultCacheDir() string {
 	if envGOPATH := os.Getenv("GOPATH"); envGOPATH != "" {
 		gopath := strings.Split(envGOPATH, string(os.PathListSeparator))
 		return filepath.Join(gopath[0], "pkg", "testtime")
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/tenntenn/testtime
 
-go 1.16
+go 1.23.1
 
 require golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e
 
